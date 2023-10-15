65d25f20b6fc3112cd7e01609995c27737edc863
brtashp <brta.shp@gmail.com>
2023-10-15 16:14:40 -0400

    update 1

--- Project.R
+++ Project.R
@@ -1,8 +1,16 @@
-# Stat data intellegence 
+# Stat data intelligence project - predict the final price of the home 
+# 
 
 # load data 
 
 data = read.csv('train.csv')
-
 str(data)
+summary(data)
 
+# data cleaning 
+# need to sort char data into int values (categorial to numerical values)
+# first by assigning values to char condition ratings 
+
+library(ggplot2)
+
+hist(data$SalePrice)
