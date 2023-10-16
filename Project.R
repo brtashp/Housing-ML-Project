7f0efe37a4200a486398c3424c116f42dfb48db2
brtashp <brta.shp@gmail.com>
2023-10-15 21:58:45 -0400

    re-attempt at for loops
    not really working? might need to go back one

--- Project.R
+++ Project.R
@@ -3,7 +3,8 @@
 # libraries: 
 library(ggplot2)
 
-
+# notes: 
+# need to remove id since its not really necessary
 
 # load data 
 data = read.csv('train.csv')
@@ -18,16 +19,30 @@ library(ggplot2)
 
 hist(data$SalePrice)
 
+numerical_data <- data[sapply(data, is.numeric)]
+
 for (column in names(numerical_data)) {
-  p <- ggplot(data, aes(x = data[[column]])) +
-    geom_histogram(binwidth = 5, fill = "blue", color = "black") +
-    labs(x = "X-axis Label", y = "Frequency", title = paste("Histogram for", column)) +
-    theme_minimal()
-  
-  # Use print() to display each plot individually
-  print(p)
-  
-  # Save the plot as an image
-  ggsave(paste(column, "_histogram.png"), plot = p, width = 6, height = 4)
+  if (!is.null(data[[column]]) && !anyNA(data[[column]])) {
+    
+    # Calculate an appropriate binwidth using Scott's Rule
+    binwidth <- diff(range(data[[column]])) / (3.5 * IQR(data[[column]]) / length(data[[column]])^(1/3))
+    
+    # Check if the binwidth is non-finite or very small
+    if (!is.finite(binwidth) || binwidth < 1e-6) {
+      # Set a reasonable binwidth for very small ranges
+      binwidth <- 1
+    }
+    
+    p <- ggplot(data, aes(x = .data[[column]])) +
+      geom_histogram(binwidth = binwidth, fill = "blue", color = "black") +
+      labs(x = "X-axis Label", y = "Frequency", title = paste("Histogram for", column)) +
+      theme_minimal()
+    
+    # Use print() to display each plot individually
+    print(p)
+    
+    # Save the plot as an image
+    ggsave(paste(column, "_histogram.png"), plot = p, width = 6, height = 4)
+  }
 }
 
