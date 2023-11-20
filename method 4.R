49b24c67f452f31dc08176c864164af0cd0f41eb
Britney Shepherd <brta.shp@gmail.com>
2023-11-20 13:37:02 -0500

    updated methods with code, havent checked if  3,4,5 works

--- method 4.R
+++ method 4.R
@@ -1,1 +1,144 @@
 # method 4
+# normalizing data, revaluing some features, 
+
+# load libraries 
+library(caTools)
+library(randomForest)
+library(dplyr)
+library(ggplot2)
+#install.packages("PerformanceAnalytics")
+library(PerformanceAnalytics)
+
+# loading data 
+dataTrain = read.csv("train.csv")
+dataTest = read.csv("test.csv")
+# remove unnecessary columns 
+dataTrain$Id = NULL
+dataTest$Id = NULL
+dataTrain$PoolQC = NULL
+dataTest$PoolQC = NULL
+dataTrain$MiscFeature = NULL
+dataTest$MiscFeature = NULL
+dataTrain$Alley = NULL
+dataTest$Alley = NULL
+dataTrain$Fence = NULL
+dataTest$Fence = NULL
+dataTrain$MSZoning = NULL
+dataTest$MSZoning = NULL
+
+#SalePrice = dataTrain$SalePrice
+
+SalePrice1 <- log(dataTrain$SalePrice)
+SalePrice <- log(dataTrain$SalePrice)
+dataTrain$SalePrice = SalePrice1
+
+# cleaning Train and test
+character_columns = sapply(dataTrain, is.character)
+dataTrainChar = dataTrain[, character_columns]
+
+character_columns = sapply(dataTrain, is.character)
+dataTestChar = dataTest[, character_columns]
+
+#below works for test
+for (col_name in names(dataTrainChar)) {
+  mean_sale_price = tapply(SalePrice, dataTrainChar[[col_name]], mean)
+  
+  # Use a different variable name for rank
+  rank_values = rank(mean_sale_price)
+  
+  # Rename the values in dataTestChar based on rank
+  dataTestChar[[col_name]] <- ifelse(!is.na(match(dataTestChar[[col_name]], names(mean_sale_price))), 
+                                     rank_values[match(dataTestChar[[col_name]], names(mean_sale_price))], 
+                                     dataTestChar[[col_name]])
+  dataTestChar[[col_name]] = as.numeric(dataTestChar[[col_name]])
+}
+# below works for train 
+for (col_name in names(dataTrainChar)) {
+  # If the column is a factor, calculate the mean SalePrice for each unique value
+  mean_sale_price = tapply(SalePrice, dataTrainChar[[col_name]], mean)
+  #ordered = order(mean_sale_price)
+  rank = rank(mean_sale_price)
+  
+  # Add the results to the result data frame
+  dataTrainChar[[col_name]] <- ifelse(!is.na(match(dataTrainChar[[col_name]], names(rank))), 
+                                      rank[match(dataTrainChar[[col_name]], names(rank))], 
+                                      dataTrainChar[[col_name]])
+  dataTrainChar[[col_name]] = as.numeric(dataTrainChar[[col_name]])
+}
+
+# creates dataframe with only numeric/int values for train 
+missing_columns = setdiff(names(dataTrain), names(dataTrainChar))
+dataTrainNum <- dataTrain[, missing_columns]
+# removes the sale price column
+dataTrainNum$SalePrice = NULL
+
+# below for cleaning test data
+# creates dataframe with only numeric/int values 
+missing_columns = setdiff(names(dataTest), names(dataTestChar))
+dataTestNum <- dataTest[, missing_columns]
+
+# combines everything into one dataframe for train data 
+dataTrainAll = cbind(dataTrainChar, dataTrainNum)
+dataTrainAll$SalePrice = dataTrain$SalePrice
+
+# corrects NAs for train data
+dataTrainAll[] <- lapply(dataTrainAll, function(x) {
+  ifelse(is.na(x), mean(x, na.rm = TRUE), x)
+})
+
+# combines everything into one dataframe 
+dataTestAll = cbind(dataTestChar, dataTestNum)
+
+# changes NA to mean 
+dataTestAll <- dataTestAll %>%
+  mutate(across(everything(), ~ ifelse(is.na(.), mean(., na.rm = TRUE), .)))
+
+highcorTrain = dataTrainAll[, c(7,17,19,20,29,33,42,44,45,50,51,54,57,61,64,65,75)]
+highcorTest = dataTestAll[, c(7,17,19,20,29,33,42,44,45,50,51,54,57,61,64,65)]
+
+# testing the accuracy (need to use the train data to get the accuracy) ###
+startModel = randomForest(SalePrice ~ ., data = dataTrainAll, ntree = 500)
+
+# Predict sale prices for the test dataset
+#predictions = predict(startModel, newdata = dataTestAll)
+predictions1 = predict(startModel, newdata = dataTestAll)
+predictions = exp(predictions1)
+
+IDnum = 1461:2919
+MySubmission = data.frame(Id = IDnum, SalePrice = predictions)
+write.csv(MySubmission, "predictionsRandomForest.csv", row.names=FALSE)
+
+# results of the model #########################################################
+
+# split train data into own train and test to test model 
+# split data into train and test (to test model)
+set.seed(88)
+split = sample.split(dataTrainAll$SalePrice, SplitRatio = 0.75)
+resultsdataTrain = subset(dataTrainAll, split == TRUE)
+resultsdataTest = subset(dataTrainAll, split == FALSE)
+resultsdataTest$SalePrice = NULL
+
+# rerun model using new test and train data
+startModelTest = randomForest(SalePrice ~ ., data = resultsdataTrain, ntree = 500)
+predictionTest = predict(startModelTest, newdata = resultsdataTest)
+
+# mae results 
+mae = mean(abs(resultsdataTrain$SalePrice - predictionTest))
+# mse results
+# Assuming y_true and y_pred are your actual and predicted values
+mse = mean((resultsdataTrain$SalePrice - predictionTest)^2)
+# Calculate the Root Mean Squared Error (RMSE)
+rmse = sqrt(mean((resultsdataTrain$SalePrice - predictionTest)^2))
+# mape results
+mape = mean(abs((resultsdataTrain$SalePrice - predictionTest) / resultsdataTrain$SalePrice)) * 100
+# Define a threshold for acceptable error
+threshold = 10000
+# Calculate accuracy as the percentage of predictions within the threshold
+accuracy = mean(abs(resultsdataTrain$SalePrice - predictionTest) < threshold)
+
+# Print results
+cat("Mean Absolute Error (MAE): ", mae, "\n")
+cat("Mean Squared Error (MSE): ", mse, "\n")
+cat("Root Mean Squared Error (RMSE): ", rmse, "\n")
+cat("Mean Absolute Percentage Error (MAPE): ", mape, "\n")
+cat("Accuracy within $", threshold, ": ", accuracy * 100, "%\n")
